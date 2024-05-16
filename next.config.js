024fba6aba8f0888c79a453d56d28801416be2e4
kriptonian1 <shymal.sawan@gmail.com>
2024-05-16 20:12:35 +0530

    feat: picture frame

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-const withNextra = require('nextra')({
-	theme: 'nextra-theme-docs',
-	themeConfig: './theme.config.tsx',
-  })
-  
-  module.exports = withNextra()
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+	remotePatterns:[
+		{
+			protocol: 'https',
+			hostname: "images.unsplash.com"
+		}
+	]
+  }
+};
+
+module.exports = withNextra(nextConfig);
